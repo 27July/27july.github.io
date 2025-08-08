da193f6893cbe34bd50e9f64e9fb5d9120a669a6
Zi Hao Wee <91110126+27July@users.noreply.github.com>
2025-08-09 00:08:35 +0800

    next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // Static site for github pages
+  output: "export",
+  // Disable SSR since github pages static only
+  images: {
+    unoptimized: true,
+  },
+  // Enable trailing slash for GitHub Pages compatibility
+  trailingSlash: true,
+
 };
 
 export default nextConfig;
